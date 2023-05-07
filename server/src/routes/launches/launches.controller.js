884ce1778033e5f447595f7044f111c60beba25e
milan10110 <meelaanvaghasiya00@gmail.com>
2023-05-07 15:06:25 +0530

    Update getAllLaunches to fetch launches from mongoDB

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,8 +5,9 @@ const {
   existsLaunchWithId,
 } = require("../../models/launches.modal");
 
-function httpGetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+  // return res.status(200).json(getAllLaunches());
+  return await getAllLaunches();
 }
 
 function httpAddNewLaunch(req, res) {
